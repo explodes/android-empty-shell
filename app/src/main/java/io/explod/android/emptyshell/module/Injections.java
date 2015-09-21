f59c37b6a426d43ca8631f2b2dc69529cb034da3
Evan Leis <evan.explodes@gmail.com>
2015-09-21 00:02:39 -0600

    [main] main fragment w/ butterknife, inject, newInstance example

--- app/src/main/java/io/explod/android/emptyshell/module/Injections.java
+++ app/src/main/java/io/explod/android/emptyshell/module/Injections.java
@@ -1,6 +1,7 @@
 package io.explod.android.emptyshell.module;
 
 import io.explod.android.emptyshell.ui.activity.MainActivity;
+import io.explod.android.emptyshell.ui.fragment.MainFragment;
 import io.explod.android.emptyshell.ui.widget.typeface.CustomTypefaceSpan;
 import io.explod.android.emptyshell.ui.widget.typeface.TypefaceButton;
 import io.explod.android.emptyshell.ui.widget.typeface.TypefaceEditText;
@@ -18,4 +19,5 @@ public interface Injections {
 
 	void inject(MainActivity target);
 
+	void inject(MainFragment target);
 }
--- app/src/main/java/io/explod/android/emptyshell/ui/activity/MainActivity.java
+++ app/src/main/java/io/explod/android/emptyshell/ui/activity/MainActivity.java
@@ -1,17 +1,21 @@
 package io.explod.android.emptyshell.ui.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import io.explod.android.emptyshell.R;
+import io.explod.android.emptyshell.ui.fragment.MainFragment;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		if (savedInstanceState == null) {
+			openFragment(MainFragment.newInstance(), false);
+		}
 	}
 
 }
