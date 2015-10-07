21acc910f6d237f30c3b964c08dffdb67a534e53
Evan Leis <evan.explodes@gmail.com>
2015-10-07 16:02:01 -0600

    [retrofit] retrofit 2.0.0-beta2

--- app/src/main/java/io/explod/android/emptyshell/module/modules/NetworkModule.java
+++ app/src/main/java/io/explod/android/emptyshell/module/modules/NetworkModule.java
@@ -3,9 +3,10 @@ package io.explod.android.emptyshell.module.modules;
 import android.app.Application;
 import android.os.Build;
 
-import com.google.gson.Gson;
 import com.squareup.okhttp.Cache;
+import com.squareup.okhttp.Interceptor;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
 
 import java.io.File;
 import java.util.HashMap;
@@ -15,23 +16,18 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
-import io.explod.android.emptyshell.BuildConfig;
 import io.explod.android.emptyshell.Constants;
 import io.explod.android.emptyshell.network.ApiEndpoint;
 import io.explod.android.emptyshell.network.AppAuthorizedService;
 import io.explod.android.emptyshell.network.AppService;
 import io.explod.android.emptyshell.network.mock.MockAppService;
 import io.explod.android.emptyshell.util.prefs.EnumPreference;
-import retrofit.RequestInterceptor;
-import retrofit.RestAdapter;
-import retrofit.client.OkClient;
-import retrofit.converter.Converter;
-import retrofit.converter.GsonConverter;
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
+import retrofit.RxJavaCallAdapterFactory;
 
 @Module
 public class NetworkModule {
-
-	private static final String TAG = "NetworkModule";
 
 	private static final int HTTP_DISK_CACHE_SIZE = 1024 * 1024 * 50; // 50MB
 
@@ -42,8 +38,7 @@ public class NetworkModule {
 	private HashMap<ApiEndpoint, AppAuthorizedService> mAuthorizedServices = new HashMap<>(ApiEndpoint.values().length);
 
 	@Provides
-	AppService provideAppService(OkClient okClient,
-								 Gson gson,
+	AppService provideAppService(OkHttpClient okHttpClient,
 								 @Named(PreferenceModule.PREF_API_ENDPOINT) EnumPreference<ApiEndpoint> apiEndpointPref) {
 		ApiEndpoint endpoint = apiEndpointPref.get();
 		AppService service = mServices.get(endpoint);
@@ -53,14 +48,18 @@ public class NetworkModule {
 				service = new MockAppService();
 			} else {
 				final String userAgent = createUserAgent();
-				RequestInterceptor interceptor = request -> {
-					request.addHeader(Constants.HTTP_HEADER_USER_AGENT, userAgent);
-					request.addHeader(Constants.HTTP_HEADER_ACCEPT, Constants.HTTP_MIME_JSON);
-					request.addHeader(Constants.HTTP_HEADER_CONTENT_TYPE, Constants.HTTP_MIME_JSON);
-					request.addHeader(Constants.HTTP_HEADER_ACCEPT_ENCODING, Constants.HTTP_ACCEPT_ENCODING_GZIP);
-					request.addHeader(Constants.HTTP_HEADER_ACCEPT_CHARSET, Constants.HTTP_ACCEPT_CHARSET_UTF8);
+				Interceptor interceptor = chain -> {
+					Request original = chain.request();
+					Request request = original.newBuilder()
+						.header(Constants.HTTP_HEADER_USER_AGENT, userAgent)
+						.header(Constants.HTTP_HEADER_ACCEPT, Constants.HTTP_MIME_JSON)
+						.header(Constants.HTTP_HEADER_CONTENT_TYPE, Constants.HTTP_MIME_JSON)
+						.header(Constants.HTTP_HEADER_ACCEPT_ENCODING, Constants.HTTP_ACCEPT_ENCODING_GZIP)
+						.header(Constants.HTTP_HEADER_ACCEPT_CHARSET, Constants.HTTP_ACCEPT_CHARSET_UTF8)
+						.build();
+					return chain.proceed(request);
 				};
-				service = createService(okClient, gson, interceptor, endpoint.serviceHost, AppService.class);
+				service = createService(okHttpClient, interceptor, endpoint.serviceHost, AppService.class);
 			}
 			mServices.put(endpoint, service);
 		}
@@ -68,8 +67,7 @@ public class NetworkModule {
 	}
 
 	@Provides
-	AppAuthorizedService provideAppAuthorizedService(OkClient okClient,
-													 Gson gson,
+	AppAuthorizedService provideAppAuthorizedService(OkHttpClient okHttpClient,
 													 @Named(PreferenceModule.PREF_API_ENDPOINT) EnumPreference<ApiEndpoint> apiEndpointPref) {
 		ApiEndpoint endpoint = apiEndpointPref.get();
 		AppAuthorizedService service = mAuthorizedServices.get(endpoint);
@@ -79,51 +77,47 @@ public class NetworkModule {
 				service = new MockAppService();
 			} else {
 				final String userAgent = createUserAgent();
-				RequestInterceptor interceptor = request -> {
+				Interceptor interceptor = chain -> {
+					Request original = chain.request();
+					Request.Builder requestBuilder = original.newBuilder();
 //					Token.Response token = tokenPref.getObject();
 //					if (!Token.Response.isValid(token)) {
 //						doLogout(context);
 //						return;
 //					} else {
 //						String authorization = Constants.HTTP_HEADER_AUTHORIZATION_BEARER + " " + token.accessToken;
-//						request.addHeader(Constants.HTTP_HEADER_AUTHORIZATION, authorization);
+//						requestBuilder.header(Constants.HTTP_HEADER_AUTHORIZATION, authorization);
 //					}
-					request.addHeader(Constants.HTTP_HEADER_USER_AGENT, userAgent);
-					request.addHeader(Constants.HTTP_HEADER_ACCEPT, Constants.HTTP_MIME_JSON);
-					request.addHeader(Constants.HTTP_HEADER_CONTENT_TYPE, Constants.HTTP_MIME_JSON);
-					request.addHeader(Constants.HTTP_HEADER_ACCEPT_ENCODING, Constants.HTTP_ACCEPT_ENCODING_GZIP);
-					request.addHeader(Constants.HTTP_HEADER_ACCEPT_CHARSET, Constants.HTTP_ACCEPT_CHARSET_UTF8);
+					Request request = requestBuilder.header(Constants.HTTP_HEADER_USER_AGENT, userAgent)
+						.header(Constants.HTTP_HEADER_ACCEPT, Constants.HTTP_MIME_JSON)
+						.header(Constants.HTTP_HEADER_CONTENT_TYPE, Constants.HTTP_MIME_JSON)
+						.header(Constants.HTTP_HEADER_ACCEPT_ENCODING, Constants.HTTP_ACCEPT_ENCODING_GZIP)
+						.header(Constants.HTTP_HEADER_ACCEPT_CHARSET, Constants.HTTP_ACCEPT_CHARSET_UTF8)
+						.build();
+
+					return chain.proceed(request);
 				};
-				service = createService(okClient, gson, interceptor, endpoint.serviceHost, AppAuthorizedService.class);
+				service = createService(okHttpClient, interceptor, endpoint.serviceHost, AppAuthorizedService.class);
 			}
 			mAuthorizedServices.put(endpoint, service);
 		}
 		return service;
 	}
 
-	private static <T> T createService(OkClient okClient,
-									   Gson gson,
-									   RequestInterceptor interceptor,
+	private static <T> T createService(OkHttpClient okHttpClient,
+									   Interceptor interceptor,
 									   String host,
 									   Class<? extends T> serviceClass) {
-		Converter converter = new GsonConverter(gson);
-		RestAdapter.Builder builder = new RestAdapter.Builder()
-			.setEndpoint(host)
-			.setClient(okClient)
-			.setConverter(converter)
-			.setLogLevel(RestAdapter.LogLevel.BASIC)
-			.setRequestInterceptor(interceptor);
-		if (BuildConfig.DEBUG) {
-			builder.setLogLevel(RestAdapter.LogLevel.FULL);
-		}
-		RestAdapter adapter = builder.build();
-		return adapter.create(serviceClass);
-	}
-
-	@Provides
-	@Singleton
-	OkClient provideOkClient(OkHttpClient okHttpClient) {
-		return new OkClient(okHttpClient);
+		// add the auth / header interceptor as the first interceptor
+		okHttpClient.interceptors().add(0, interceptor);
+		// build the retrofit service
+		return new Retrofit.Builder()
+			.baseUrl(host)
+			.client(okHttpClient)
+			.addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+			.addConverterFactory(GsonConverterFactory.create())
+			.build()
+			.create(serviceClass);
 	}
 
 	@Provides
